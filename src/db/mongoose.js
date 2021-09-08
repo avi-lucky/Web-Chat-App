f03250e4833d32f43c9cf4472015bfedf83dc787
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-09 00:33:49 +0530

    Add Friends UI

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
 
-// mongoose.set("useFindAndModify", false)
-// mongoose.set("useUnifiedTopology", true)
-// mongoose.set("useCreateIndex", true)
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
 // console.log(process.env.DB_CONNECT);
 })
