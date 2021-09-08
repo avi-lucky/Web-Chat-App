2669fc66f890ad3a674610eb3e24e2eaec8d7648
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-09 02:40:08 +0530

    All Friends Listed

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,21 @@
-// axios.get('/tasks', {
-//     headers: {
-//       Authorization: ('Bearer ', localStorage.getItem("token"))
-//     },
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//     console.log(response.data)
-//   })
+axios.get('/friends', {
+    headers: {
+      Authorization: ('Bearer ', localStorage.getItem("token"))
+    },
+  })
+  .then(function (response) {
+    var friends = ''
+    for (i = 0; i < response.data.length; i++) 
+    {
+      id = response.data[i]._id
+      console.log(id)
+      friends += `<div class="row-8" id="${id}"><h4>${response.data[i].name}</h4></div>`
+    }
+    document.getElementById('friends').innerHTML = friends
+  })
+  .catch(function (error) {
+    console.log(error)
+  });
 
 // Logout User
 function logOut() {
