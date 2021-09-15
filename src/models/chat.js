d51cd75c3f7b9a0e7dd851c61d2ac108a133f474
Avikal Shukla <shuklaavikal7@gmail.com>
2021-09-15 20:06:01 +0530

    Chats Stored In Database

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,27 +1,18 @@
 const mongoose = require('mongoose')
 
 const chatSchema = new mongoose.Schema({
-    chat: [{
-        sender: {
-            type: mongoose.Schema.Types.String,
-            required: true,
-            trim: true,
-            ref: 'User'
-        },
-        receiver: {
-            type: mongoose.Schema.Types.String,
-            required: true,
-            trim: true,
-            ref: 'Friend'
-        },
-        messages: {
-            type: String,
-            required: true,
-        },
-    }, {
-        timestamps: true
+    sender: {
+        type: String,
+        required: true,
+    },
+    receiver: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
     }
-]
 })
 
 const Chat = mongoose.model('Chat', chatSchema)
--- src/routers/chat.js
+++ src/routers/chat.js
@@ -4,4 +4,18 @@ const Chat = require('../models/chat')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
+router.post('/chats', auth, async (req, res) => {
+    const chat = new Chat ({
+        sender: req.user.email,
+        receiver: req.body.friend,
+        message: req.body.message
+    })
+    try {
+        await chat.save()
+        res.status(201).send(chat)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 module.exports = router
